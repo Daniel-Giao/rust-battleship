4b86a534fe655b4e969d425c7ee834d4f365ca50
Daniel Giao <daniel.giao@icloud.com>
2024-08-14 22:48:45 -0400

    place boat and placing verification logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use rand::Rng;
-use std::io::{self, Write};
+use std::{
+    intrinsics::simd::simd_reduce_all,
+    io::{self, Write},
+};
 
 const BOARD_SIZE: usize = 10;
 
@@ -22,6 +25,51 @@ impl Board {
             grid: [[CellState::Empty; BOARD_SIZE]; BOARD_SIZE],
             ships: Vec::new(),
         }
+    }
+
+    fn place_ship(&mut self, size: usize) {
+        let mut rng = rand::thread_rng();
+        loop {
+            let row = rng.gen_range(0..BOARD_SIZE);
+            let col = rng.gen_range(0..BOARD_SIZE);
+            let is_horizontal = rng.gen::<bool>();
+
+            if self.can_place_ship(row, col, size, is_horizontal) {
+                for i in 0..size {
+                    let (r, c) = if is_horizontal {
+                        (row, col + i)
+                    } else {
+                        (row + i, col)
+                    };
+                    self.grid[r][c] = CellState::Ship;
+                    self.ships.push((r,c))
+                }
+                break;
+            }
+        }
+    }
+
+    fn can_place_ship(&self, row: usize, col: usize, size: usize, is_horizontal: bool) -> bool {
+        if is_horizontal {
+            if col + size > BOARD_SIZE {
+                return false;
+            }
+            for i in 0..size {
+                if self.grid[row][col + i] != CellState::Empty {
+                    return false;
+                }
+            }
+        } else {
+            if row + size > BOARD_SIZE {
+                return false;
+            }
+            for i in 0..size {
+                if self.grid[row + i][col] != CellState::Empty {
+                    return false;
+                }
+            }
+        }
+        true
     }
 }
 
